e019ee54d1aaaea437c14908dfdc2b3761f3891e
sairas774 <kurono_guren@yahoo.co.jp>
2021-05-30 20:59:56 +0900

    テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,6 +17,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
+      it 'userが紐付いていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
       it 'nameが空では登録できない' do
         @item.name = ''
         @item.valid?
